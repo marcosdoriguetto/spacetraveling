10e8870272912c1f003cd348d6218c94cb438205
Marcos <marcosdoriguetto@gmail.com>
2022-07-05 10:48:55 -0300

    Create dateFormat helper and update home page

---
+++ src/helpers/dateFormat.ts
@@ -0,0 +1,7 @@
+import { format } from 'date-fns'
+
+export function dateFormat(date: string | null) {
+  if (date) {
+    return format(new Date(date), 'dd MMM yyyy')
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,102 @@
-import { GetStaticProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Link from 'next/link'
 
-import { getPrismicClient } from '../services/prismic';
+import { useState } from 'react'
 
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
+import { AiOutlineCalendar } from 'react-icons/ai'
+import { BsPerson } from 'react-icons/bs'
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
+import { getPrismicClient } from '../services/prismic'
+
+import { dateFormat } from '../helpers/dateFormat'
+
+import { HomeProps, Post } from './Home.interface'
+
+import styles from './home.module.scss'
+
+export default function Home({ postsPagination }: HomeProps) {
+  const [nextPage, setNextPage] = useState<string | null>(
+    postsPagination.next_page
+  )
+  const [posts, setPosts] = useState(postsPagination.results)
+
+  function loadMorePosts() {
+    fetch(nextPage as string)
+      .then(res => res.json())
+      .then(data => {
+        setNextPage(data.next_page)
+        setPosts(prevState => [
+          ...prevState,
+          ...data.results.map((result: Post) => ({
+            ...result,
+            first_publication_date: dateFormat(result.first_publication_date)
+          }))
+        ])
+      })
+  }
+
+  return (
+    <main>
+      <section className={styles.posts}>
+        {posts.map(post => (
+          <Link key={post.uid} href={`/post/${post.uid}`}>
+            <a>
+              <div className={styles.postContainer}>
+                <div className={styles.postHeader}>
+                  <h2>{post.data.title}</h2>
+                  <p>{post.data.subtitle}</p>
+                </div>
+
+                <div className={styles.postFooter}>
+                  <span>
+                    <AiOutlineCalendar fontSize="1.25rem" />
+                    {post.first_publication_date}
+                  </span>
+                  <span>
+                    <BsPerson fontSize="1.25rem" /> {post.data.author}
+                  </span>
+                </div>
+              </div>
+            </a>
+          </Link>
+        ))}
+      </section>
+
+      {nextPage && (
+        <span className={styles.loadMorePosts} onClick={loadMorePosts}>
+          Carregar mais posts
+        </span>
+      )}
+    </main>
+  )
 }
 
-interface PostPagination {
-  next_page: string;
-  results: Post[];
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
 }
 
-interface HomeProps {
-  postsPagination: PostPagination;
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({})
+
+  const response = await prismic.getByType('posts', {
+    pageSize: 1
+  })
+
+  const postsPagination = {
+    next_page: response.next_page,
+    results: response.results.map(result => ({
+      ...result,
+      first_publication_date: dateFormat(result.first_publication_date)
+    }))
+  }
+
+  return {
+    props: {
+      postsPagination
+    },
+    revalidate: 30 * 60 // 30 minutes
+  }
 }
-
-// export default function Home() {
-//   // TODO
-// }
-
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
-
-//   // TODO
-// };
